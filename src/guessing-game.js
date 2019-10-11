a24094143710232105d4a8a8cb3567837b9b2462
fcsmandrei <leonchikovandrei@gmail.com>
2019-10-11 17:14:21 +0300

    feat: complete task guessing-game

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,26 @@
 class GuessingGame {
-    constructor() {}
+    constructor() {
+        this.minValue = 0;
+        this.maxValue = 0;
+        this.middle = 0;
+    }
 
     setRange(min, max) {
-
+        this.minValue = min;
+        this.maxValue = max;
     }
 
     guess() {
-
+        this.middle = Math.round ((this.minValue + this.maxValue) / 2);
+        return this.middle;
     }
 
     lower() {
-
+        this.maxValue = this.middle;
     }
 
     greater() {
-
+        this.minValue = this.middle;
     }
 }
 
